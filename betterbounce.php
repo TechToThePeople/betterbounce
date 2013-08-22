a61d8d6015f7dc777413f227701328afb51a4c64
Xavier Dutoit <xavier@tttp.eu>
2013-08-22 09:14:07 +0200

    everything ready

--- betterbounce.php
+++ betterbounce.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once 'bounce2activity.civix.php';
+require_once 'betterbounce.civix.php';
 
 /**
  * Implementation of hook_civicrm_config
  */
 
-function bounce2activity_civicrm_apiWrapper (&$wrappers,$apiRequest) {
+function betterbounce_civicrm_apiWrapper (&$wrappers,$apiRequest) {
   if ("event_bounce" != $apiRequest ["action"] || "mailing" != $apiRequest ["entity"]) {
 //    return; 
   }
@@ -17,8 +17,8 @@ function bounce2activity_civicrm_apiWrapper (&$wrappers,$apiRequest) {
 /**
  * Implementation of hook_civicrm_config
  */
-function bounce2activity_civicrm_config(&$config) {
-  _bounce2activity_civix_civicrm_config($config);
+function betterbounce_civicrm_config(&$config) {
+  _betterbounce_civix_civicrm_config($config);
 }
 
 /**
@@ -26,36 +26,38 @@ function bounce2activity_civicrm_config(&$config) {
  *
  * @param $files array(string)
  */
-function bounce2activity_civicrm_xmlMenu(&$files) {
-  _bounce2activity_civix_civicrm_xmlMenu($files);
+function betterbounce_civicrm_xmlMenu(&$files) {
+  _betterbounce_civix_civicrm_xmlMenu($files);
 }
 
 /**
  * Implementation of hook_civicrm_install
  */
-function bounce2activity_civicrm_install() {
-  return _bounce2activity_civix_civicrm_install();
+function betterbounce_civicrm_install() {
+  $r = civicrm_api("ActivityType","create",array("version"=>3,"name"=>"bounce","label"=>"Bounce", "is_reserved"=>1,"is_active"=>1,"component_id"=>4,"description"=>"mail bounced after a mailing","weight"=>1));
+  print_r($r);
+  return _betterbounce_civix_civicrm_install();
 }
 
 /**
  * Implementation of hook_civicrm_uninstall
  */
-function bounce2activity_civicrm_uninstall() {
-  return _bounce2activity_civix_civicrm_uninstall();
+function betterbounce_civicrm_uninstall() {
+  return _betterbounce_civix_civicrm_uninstall();
 }
 
 /**
  * Implementation of hook_civicrm_enable
  */
-function bounce2activity_civicrm_enable() {
-  return _bounce2activity_civix_civicrm_enable();
+function betterbounce_civicrm_enable() {
+  return _betterbounce_civix_civicrm_enable();
 }
 
 /**
  * Implementation of hook_civicrm_disable
  */
-function bounce2activity_civicrm_disable() {
-  return _bounce2activity_civix_civicrm_disable();
+function betterbounce_civicrm_disable() {
+  return _betterbounce_civix_civicrm_disable();
 }
 
 /**
@@ -67,8 +69,8 @@ function bounce2activity_civicrm_disable() {
  * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
  *                for 'enqueue', returns void
  */
-function bounce2activity_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _bounce2activity_civix_civicrm_upgrade($op, $queue);
+function betterbounce_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _betterbounce_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -77,6 +79,6 @@ function bounce2activity_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
  * Generate a list of entities to create/deactivate/delete when this module
  * is installed, disabled, uninstalled.
  */
-function bounce2activity_civicrm_managed(&$entities) {
-  return _bounce2activity_civix_civicrm_managed($entities);
+function betterbounce_civicrm_managed(&$entities) {
+  return _betterbounce_civix_civicrm_managed($entities);
 }
